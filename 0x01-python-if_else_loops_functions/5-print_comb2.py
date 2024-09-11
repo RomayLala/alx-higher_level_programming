adab5f7ea36c947904753564f5aca35bbf9729b8
RomayLala <asune.rosemary@yahoo.com>
2024-09-11 01:03:15 -0700

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-# Print numbers from 00 to 99, separated by ", "
-
 for i in range(100):
-    if i != 99:
-        print(f"{i:02d}, ", end="")
-    else:
-        print(f"{i:02d}")
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
