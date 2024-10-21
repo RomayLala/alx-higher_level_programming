8b904ebc5f7b814541be39b4a7557b4b9de0bf1f
RomayLala <asune.rosemary@gmail.com>
2024-10-21 11:27:05 -0700

    python almost a circle

--- 0x0C-python-almost_a_circle/README.md
+++ 0x0C-python-almost_a_circle/README.md
@@ -1,7 +1,24 @@
 This function is the first class Base.
+
 This function is the class Rectangle that inherits from Base.
+
 This function updates the class Rectangle by adding validation of all setter methods and instantiation (id excluded).
+
 This function updates the class Rectangle by adding the public method def area(self): that returns the area value of the Rectangle instance.
+
 This function updates the class Rectangle by adding the public method def display(self): that prints in stdout the Rectangle instance with the character # without handling x and y.
+
 This function updates the class Rectangle by overriding the __str__ method so that it returns [Rectangle] (<id>) <x>/<y> - <width>/<height>.
+
+This function updates  the class Rectangle by improving the public method def display(self): to print in stdout the Rectangle instance with the character # by taking care of x and y.
+
+This function updates the class Rectangle by adding the public method def update(self, *args): that assigns an argument to each attribute:
+1st argument should be the id attribute
+2nd argument should be the width attribute
+3rd argument should be the height attribute
+4th argument should be the x attribute
+5th argument should be the y attribute
+
+This function updates the class Rectangle by updating the public method def update(self, *args): by changing the prototype to update(self, *args, **kwargs) that assigns a key/value argument to attributes.
+
 
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,9 +93,10 @@ class Rectangle:
         return self.width * self.height
 
     def display(self):
-        """Prints the rectangle instance using the `#` character."""
+        """Prints the rectangle instance using the `#` character, taking x and y offsets into account."""
+        print("\n" * self.y, end="")  # Print y empty lines
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """Return a string representation of the Rectangle instance.
@@ -104,3 +105,41 @@ class Rectangle:
             str: A formatted string in the form [Rectangle] (<id>) <x>/<y> - <width>/<height>
         """
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+    def update(self, *args, **kwargs):
+        """Update the attributes of the Rectangle instance using no-keyword and keyword arguments.
+
+        Args:
+            args (tuple): A variable number of positional arguments to update attributes:
+                1. id (int): The new id of the rectangle.
+                2. width (int): The new width of the rectangle.
+                3. height (int): The new height of the rectangle.
+                4. x (int): The new x coordinate of the rectangle.
+                5. y (int): The new y coordinate of the rectangle.
+            kwargs (dict): A variable number of keyword arguments to update attributes.
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        
+        if len(args) == 0:
+            # Only use kwargs if args is empty
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+        else:
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
