ebfd2b05358c2cd96c5b2a680929a2ca1c98c25f
RomayLala <asune.rosemary@yahoo.com>
2024-11-20 23:31:29 -0800

    MySQL

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,8 @@
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
+-- Script to list all cities and their states from the hbtn_0d_usa database
+-- Each record shows city.id, city.name, and state.name
+-- Sorted by city.id in ascending order
+
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
